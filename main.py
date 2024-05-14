fc6ecaadba785e203d500f912eaf19a9c9670bb2
Jordan <jordan.amos@gmail.com>
2024-05-15 03:15:53 +1000

    Added betterer code

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import os
 import functools
 import glob
 import json
-from typing import NamedTuple
+import shutil
 
 from io import BytesIO
 from requests import Session
+from typing import NamedTuple
 from zipfile import BadZipFile
 from zipfile import ZipFile
 
 MODS_LIST_FILE_NAME = "mods.txt"
 SEP = "-"
+LEGAL_COMPANY_DIR = "/Users/jamos/Library/Containers/com.isaacmarovitz.Whisky/Bottles/8360D9E1-9B3A-4951-A7D2-099CFE706B90/drive_c/Program Files (x86)/Steam/steamapps/common/Lethal Company/"
+OUT_DIR = os.path.abspath("mods")
+
 TARGET_BASE_URL = "https://thunderstore.io/package/download"
-OUT_DIR = "mods"
 MOD_DIR_IGNORE = ("readme.md", "icon.png", "manifest.json", "changelog.md", "license")
-BEPINEX = "BepInEx-BepInExPack-5.4.2100"
+BEPINEX_PACKAGE_NAME = "BepInEx-BepInExPack-5.4.2100"
+LEGAL_COMPANY_BEPINEX_DIR = os.path.join(LEGAL_COMPANY_DIR, "BepInEx")
+LC_BEPINEX_PLUGINS_DIR = os.path.join(LEGAL_COMPANY_BEPINEX_DIR, "plugins")
+LC_BEPINEX_PATCHERS_DIR = os.path.join(LEGAL_COMPANY_BEPINEX_DIR, "patchers")
+LC_BEPINEX_CONFIG_DIR = os.path.join(LEGAL_COMPANY_BEPINEX_DIR, "config")
 
 class Mod(NamedTuple):
     by: str
@@ -44,7 +51,7 @@ class Mod(NamedTuple):
     
     @property
     def is_bepinex(self) -> str:
-        return self.package_name == BEPINEX
+        return self.package_name.casefold() == BEPINEX_PACKAGE_NAME.casefold()
     
     @property
     def bepinex_dir(self) -> str:
@@ -55,9 +62,13 @@ class Mod(NamedTuple):
         if not os.path.exists(self.bepinex_dir):
             with open(self.manifest_file, encoding="utf-8-sig") as f:
                 data = json.load(f)
-            return BEPINEX in data["dependencies"]
+            return BEPINEX_PACKAGE_NAME in data["dependencies"]
         return True
-    
+
+def _is_bepinex_installed() -> bool:
+    return os.path.exists(LEGAL_COMPANY_BEPINEX_DIR)
+
+
 def _read_mods() -> tuple[Mod]:
     with open(MODS_LIST_FILE_NAME) as f:
         mods = f.readlines()
@@ -79,6 +90,16 @@ def _download(session: Session, mod: Mod) -> int:
         print(f"Error: {format(ex)}")
         return 0
 
+def _get_out_file_path(file_to_copy: str) -> str:
+    file_to_copy = file_to_copy.replace("\\", os.sep).replace(f"BepInEx{os.sep}", "")
+    try:
+        _sub_dir = file_to_copy[:file_to_copy.index(os.sep)]
+    except ValueError:
+        file_to_copy = os.path.join("plugins", file_to_copy)
+
+    return os.path.join(LEGAL_COMPANY_BEPINEX_DIR, file_to_copy)
+
+
 def _install(mod: Mod) -> None:
     if not mod.depends_on_bepinex:
         print(f"Unable to install {mod.out_path} because it doesn't depend on BepInEx")
@@ -90,21 +111,29 @@ def _install(mod: Mod) -> None:
         and not os.path.isdir(os.path.join(mod.out_path, f))
     )
 
-
+    for file_to_copy in all:
+        out_file = _get_out_file_path(file_to_copy)
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        shutil.copy(os.path.join(mod.out_path, file_to_copy), out_file)
+        print(f"Copied {file_to_copy} to {out_file}")
 
 def _download_and_install(session: Session, mod: Mod) -> int:
     ret = _download(session, mod)
     # BepInEx Should be installed first/manually
     # if ret > 0 and not mod.is_bepinex:
-    _install(mod)
+    if not mod.is_bepinex:
+        _install(mod)
     return ret
 
 
 def main() -> int:
-    if not os.path.exists(OUT_DIR):
+    if not os.path.isdir(OUT_DIR):
         print(f"Target directory '{OUT_DIR}' does not exist.")
         return 1
     
+    if not _is_bepinex_installed():
+        print(f"{BEPINEX_PACKAGE_NAME} is not installed.")
+
     mods = _read_mods()
 
     with (
